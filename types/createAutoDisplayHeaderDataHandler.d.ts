8a624407a812d13f9665eb359968f07c339aa584
郭斌勇 <guobinyong@qq.com>
2020-05-21 11:38:55 +0800

    完善类型注释

--- types/createAutoDisplayHeaderDataHandler.d.ts
+++ types/createAutoDisplayHeaderDataHandler.d.ts
@@ -25,9 +25,11 @@ import {TemplateData,GetTemplateDataHandler} from "custom-viewbox"
 }*/
 
 
-
+/**
+ * createAutoDisplayHeaderDataHandler 的配置选项类型
+ */
 interface AutoDisplayOpts {
-    hide ? : boolean | ((vbInstance:Vue,templateData:TemplateData)=>boolean);    //可选；是否隐藏 Header
+    hide ? : boolean | ((viewBoxInst:Vue,templateData:TemplateData)=>boolean);    //可选；是否隐藏 Header
     navBarHeight ? : string;     //可选；导航条的高度
     headerHeight ? : string;     //可选；默认值是 navBarHeight ; ViewBox的Header 的高度
 } 
@@ -36,7 +38,7 @@ interface AutoDisplayOpts {
 /**
  * 自动显示ViewBox的Header 的创建者
  * 定制 Vux 的 ViewBox 组件 的 getTemplateDataHandler，使其能根据 client.showWebNavBar 和  自身是否有 header 插槽来动态决定 Vux的ViewBox的 body-padding-top 是否需要设置为 0 ；这样便不用每次使用都要单独定制了
- * @param hide ? : boolean | (vbInstance,templateData)=>boolean    可选；是否隐藏 Header
+ * @param hide ? : boolean | (viewBoxInst,templateData)=>boolean    可选；是否隐藏 Header
  * @param navBarHeight ? : string     可选；导航条的高度
  * @param headerHeight ? : string     可选；默认值是 navBarHeight ; ViewBox的Header 的高度
  *
